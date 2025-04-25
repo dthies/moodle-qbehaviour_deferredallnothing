3ea50d4ffefe1d2eb5a882e7b7022552837bf9f8
Daniel Thies <dthies@gmail.com>
2025-04-25 13:06:24 -0500

    Update for Moodle 5.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_deferredallnothing';
-$plugin->version   = 2018071304;
+$plugin->version   = 2018071305;
 
 $plugin->requires  = 2018051700;
 $plugin->dependencies = [
